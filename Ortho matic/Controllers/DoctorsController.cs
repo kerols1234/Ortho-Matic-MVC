2d54f4c4c4fd5f1db3c9ae33a2f4f4ec048a077c
kerols1234 <kerolssamer348@gmail.com>
2022-09-12 23:26:22 +0200

    add of doctor (not complete )

--- Ortho matic/Controllers/DoctorsController.cs
+++ Ortho matic/Controllers/DoctorsController.cs
@@ -37,57 +37,76 @@ namespace Ortho_matic.Controllers
             });
         }
 
-        /* [HttpGet]
-         public async Task<IActionResult> Upsert(int? id)
-         {
-             EmployeeVM employeeVM = new EmployeeVM()
-             {
-                 Employee = new Employee(),
-                 EmployeeSelectList = _db.departments.Select(i => new SelectListItem
-                 {
-                     Text = i.Name,
-                     Value = i.Id.ToString(),
-                 }),
-             };
-             if (id != null)
-             {
-                 employeeVM.Employee = await _db.employees.Include(obj => obj.Department).FirstOrDefaultAsync(obj => obj.Id == id);
-             }
-             return View(employeeVM);
-         }
+        [HttpGet]
+        public async Task<IActionResult> Upsert(int? id)
+        {
+            var doctor = new Doctor();
+            if (id != null)
+            {
+                doctor = await _context.Doctors.Include(obj => obj.DoctorClinics).Include(obj => obj.DoctorHospitals).FirstOrDefaultAsync(obj => obj.Id == id);
+            }
 
-         [HttpPost]
-         [ValidateAntiForgeryToken]
-         public IActionResult Upsert(EmployeeVM model)
-         {
-             if (ModelState.IsValid)
-             {
-                 if (model.Employee.Id != 0)
-                 {
-                     _db.employees.Update(model.Employee);
-                 }
-                 else
-                 {
-                     _db.employees.Add(model.Employee);
-                 }
-                 _db.SaveChanges();
-                 return Redirect(nameof(Index));
-             }
-             model.EmployeeSelectList = _db.departments.Select(i => new SelectListItem
-             {
-                 Text = i.Name,
-                 Value = i.Id.ToString(),
-             });
-             return View(model);
-         }
+            return View(new AddDoctorVM()
+            {
+                Doctor = doctor,
+                DoctorDegreeSelectList = Enum.GetNames(typeof(Degree)).Select(i => new SelectListItem
+                {
+                    Text = i.ToString(),
+                    Value = i.ToString(),
+                }).AsQueryable(),
+                DoctorSpecialtySelectList = Enum.GetNames(typeof(Specialty)).Select(i => new SelectListItem
+                {
+                    Text = i.ToString(),
+                    Value = i.ToString(),
+                }).AsQueryable(),
+                ClinicSelectList = _context.Clinics.Select(i => new SelectListItem
+                {
+                    Text = i.Address.ToString(),
+                    Value = i.Id.ToString(),
+                }).AsQueryable(),
+                HospitsalSelectList = _context.Hospitals.Select(i => new SelectListItem
+                {
+                    Text = i.Name.ToString(),
+                    Value = i.Id.ToString(),
+                }).AsQueryable(),
+                DaySelectList = Enum.GetNames(typeof(DayOfWeekInArabic)).Select(i => new SelectListItem
+                {
+                    Text = i.ToString(),
+                    Value = i.ToString(),
+                }).AsQueryable()
+            });
+        }
 
-         #region API Calls
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Doctor model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.Id != 0)
+                {
+                    if (!_context.Doctors.Any(obj => obj.Id == model.Id))
+                        return NotFound();
+
+                    _context.Doctors.Update(model);
+                }
+                else
+                {
+                    _context.Doctors.Add(model);
+                }
+                _context.SaveChanges();
+                return Redirect(nameof(Index));
+            }
+            return View(model);
+        }
+
+        /* #region API Calls
          [HttpGet]
-         [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-         public async Task<IActionResult> GetAll()
-         {
-             return Json(new { data = await _db.employees.Include(c => c.Department).ToListAsync() });
-         }
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.employees.Include(c => c.Department).ToListAsync() });
+        }
         */
         [HttpGet]
         public async Task<IActionResult> GetAllDoctors()
---
+++ Ortho matic/Models/ViewModels/AddDoctorVM.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace Ortho_matic.Models.ViewModels
+{
+    public class AddDoctorVM
+    {
+        public Doctor Doctor { get; set; }
+        public IEnumerable<SelectListItem> DoctorDegreeSelectList { get; set; }
+        public IEnumerable<SelectListItem> DoctorSpecialtySelectList { get; set; }
+        public IEnumerable<SelectListItem> ClinicSelectList { get; set; }
+        public IEnumerable<SelectListItem> HospitsalSelectList { get; set; }
+        public IEnumerable<SelectListItem> DaySelectList { get; set; }
+
+    }
+}
